47f9f1fa857e0961fb2f4e98efe543244afdafc7
Pranaenae <gnurugyanarp17@gmail.com>
2024-05-18 14:33:57 +0545

    Added Makefile and compose file

--- Makefile
+++ Makefile
@@ -3,3 +3,10 @@ build:
 
 run: build
 	./bin/ecom
+
+image:
+	docker build -t gojwtmysql .
+
+compose:
+	docker compose up -d --build
+  
--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/Pranaenae/gojwtmysql/cmd/api"
+	"github.com/Pranaenae/gojwtmysql/config"
+	"github.com/Pranaenae/gojwtmysql/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InitStorage(db)
+
 	server := api.NewAPIServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func InitStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB: Connected Successfully!")
 }
